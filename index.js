d43ef54e5a7c33f97c8eb5a6c1b90800783c0896
Mahran Hassan <mahrohassan60@gmail.com>
2024-04-09 00:37:41 +0100

    Your descriptive commit message here

--- index.js
+++ index.js
@@ -46,6 +46,13 @@ function renderData() {;
   
 }
 
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    fetchData();
+  }
+});
+
+
 searchButton.addEventListener('click', fetchData);
 
 
